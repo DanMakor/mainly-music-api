aa9c943cba5385a5eaa3abcfc9b8db28b65e1369
DanMakor <danielmcauliffe@outlook.com>
2021-04-19 15:10:47 +1000

    add ip address blocker

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ corsUris = [ "http://localhost:4200", "https://mainly-music-ui.herokuapp.com" ]
 app.use(cors({ origin: corsUris }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    let validIps = process.env.IPS?.split(',') || ['::12', '127.0.0.1']; // Put your IP whitelist in this array
+    console.log(validIps);
+    if(validIps.includes(req.socket.remoteAddress)){
+        // IP is ok, so go on
+        console.log("IP ok");
+        next();
+    }
+    else{
+        // Invalid ip
+        console.log("Bad IP: " + req.socket.remoteAddress);
+        const err = new Error("Bad IP: " + req.socket.remoteAddress);
+        next(err);
+    }
+})
 app.use('/api', routes);
 
 require('./mongo');
